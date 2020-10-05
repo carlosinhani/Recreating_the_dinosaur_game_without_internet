e1d82e795d8e61446f02eb34a23040d737396222
Carlos.Inhani <carlos.inhani@gmail.com>
2020-10-05 20:18:45 -0300

    add_style_JS

---
+++ script.js
@@ -0,0 +1,54 @@
+const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+let isJumping = false;
+
+function handleKeyUp(event) {
+   if (event.keyCode === 32){
+       if (!isJumping) {
+        jump();
+      }  
+   }  
+}
+
+function jump() {
+     let position = 0;
+      
+     isJumping = true;
+
+     let upInterval=setInterval(() =>{
+         if (position >= 150) {
+             clearInterval(upInterval);
+
+              // Descendo
+             let downInterval = setInterval(() =>{
+                 if (position <= 0) {
+                     clearInterval(downInterval);
+                     isJumping = false;
+                 }else {
+                    position -= 20;
+                    dino.style.bottom = position + 'px';
+                 }  
+             }, 20);
+
+         }else {
+
+             //Subindo
+            position += 20;
+            dino.style.bottom = position + 'px';
+         }
+     }, 20);
+}
+
+function createCactus() {
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    background.appendChild(cactus);
+}
+
+createCactus();
+
+document.addEventListener('keyup', handleKeyUp);
+
