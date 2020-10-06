83c2ffe63ee20f3c6f9efb9eff11274ce24a165c
Carlos.Inhani <carlos.inhani@gmail.com>
2020-10-06 11:54:10 -0300

    Finishing_the_Dino_game

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
 let isJumping = false;
+let position = 0;
 
 function handleKeyUp(event) {
    if (event.keyCode === 32){
@@ -11,8 +12,6 @@ function handleKeyUp(event) {
 }
 
 function jump() {
-     let position = 0;
-      
      isJumping = true;
 
      let upInterval=setInterval(() =>{
@@ -42,13 +41,34 @@ function jump() {
 function createCactus() {
     const cactus = document.createElement('div');
     let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
 
     cactus.classList.add('cactus');
     cactus.style.left = 1000 + 'px';
     background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+       cactusPosition -= 10;
+       cactus.style.left = cactusPosition + 'px';
+
+       if (cactusPosition < -60) {
+           clearInterval(leftInterval);
+           background.removeChild(cactus);
+
+       }else if (cactusPosition > 0 && cactusPosition < 60 && position < 60 ) {
+          // Game Over
+          clearInterval(leftInterval);
+          document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
+
+       }else {
+           cactusPosition -= 10;
+           cactus.style.left = cactusPosition + 'px';
+       }
+    }, 20);
+
+    setTimeout(createCactus, randomTime);
 }
 
 createCactus();
-
 document.addEventListener('keyup', handleKeyUp);
 
